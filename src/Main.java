0b6f851028f67e0ba93fab8bd1c2c855af48298d
vladimirivn <vladimiriv@hotmail.com>
2022-11-30 14:10:42 +0300

    ДЗ2 Задача 5

--- src/Main.java
+++ src/Main.java
@@ -40,8 +40,15 @@ public class Main {
         friend = friend / 7;
         System.out.println("friend = " + friend);
 
-
-
+        System.out.println("----Домашнее задание 2 Задача 5----");
+        var frog = 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog * 10;
+        System.out.println("frog = " + frog);
+        frog = frog / 3.5;
+        System.out.println("frog = " + frog);
+        frog = frog + 4;
+        System.out.println("frog = " + frog);
 
     }
 }
