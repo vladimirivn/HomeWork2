4bba92296822cdaf1ea4ea69a0750d2d88cec422
vladimirivn <vladimiriv@hotmail.com>
2022-11-30 14:04:16 +0300

    ДЗ1 Задача 3

--- src/Main.java
+++ src/Main.java
@@ -22,5 +22,16 @@ public class Main {
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
 
+        // ДЗ1 Задача 3
+        System.out.println("----Домашнее задание 1 Задача 3----");
+        var dog1 = 8.0;
+        dog1 -= 3.5;
+        cat -= 1.6;
+        paper -= 7639;
+        System.out.println("dog = " + dog1);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+
+
     }
 }
