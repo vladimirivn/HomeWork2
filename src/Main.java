85ef25ad17c96bd5f4c6581134ae5719d347ee49
vladimirivn <vladimiriv@hotmail.com>
2022-11-30 13:34:49 +0300

    ДЗ1 Задача 1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+
+// ДЗ1 Задача 1
+
+        var dog = 8;
+        var cat = 3.6;
+        var paper = 763789;
+        System.out.println("dog = " + dog);
+        System.out.println("dog = " + dog);
+        System.out.println("cat = " + cat);
+        System.out.println("paper = " + paper);
+
+
+    }
+}
