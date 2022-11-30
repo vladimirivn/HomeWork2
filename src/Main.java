3c71c738e9bfb49f42168cf7df3ae97bf415d7fc
vladimirivn <vladimiriv@hotmail.com>
2022-11-30 14:08:13 +0300

    ДЗ2 Задача 4

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,16 @@ public class Main {
         System.out.println("cat = " + cat);
         System.out.println("paper = " + paper);
 
+        System.out.println("----Домашнее задание 2 Задача 4----");
+        var friend = 19;
+        System.out.println("friend = " + friend);
+        friend = friend + 2;
+        System.out.println("friend = " + friend);
+        friend = friend / 7;
+        System.out.println("friend = " + friend);
+
+
+
 
     }
 }
